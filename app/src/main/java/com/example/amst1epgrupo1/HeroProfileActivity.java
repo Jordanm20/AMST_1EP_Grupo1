5aec32f7c635db25b90c894ae3763ce5588edf18
Jordanm20 <jojamira@espol.edu.ec>
2023-06-15 23:54:54 -0500

    second commit leccion

--- app/src/main/java/com/example/amst1epgrupo1/HeroProfileActivity.java
+++ app/src/main/java/com/example/amst1epgrupo1/HeroProfileActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class HeroDetailsActivity extends AppCompatActivity {
+public class HeroProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hero_details);
+        setContentView(R.layout.activity_hero_profile);
     }
 }
